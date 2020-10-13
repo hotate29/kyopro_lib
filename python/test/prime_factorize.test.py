0d88d69c8d9bd0d1888d8960fb47e6ef6a31dcab
hotate29 <44938840+hotate29@users.noreply.github.com>
2020-10-14 04:31:45 +0900

    Update prime_factorize.test.py

--- python/test/prime_factorize.test.py
+++ python/test/prime_factorize.test.py
@@ -1,14 +1,15 @@
-# verification-helper: PROBLEM https://judge.yosupo.jp/problem/factorize
+# verification-helper: PROBLEM http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_1_A
 
 
 from python.lib.prime_factrize import prime_factorize as pf
 
 
-q = int(input())
-for _ in range(q):
-  ai = int(input())
-  r = pf(ai)
-  out = ""
-  for pi,ei in r:
-    out += (str(pi)+" ")*ei
-  print(len(r),out)
+if __name__ == "__main__":
+    n = int(input())
+    r = prime_factorize(n)
+    ans = []
+    for p, e in r:
+        p = str(p)
+        for i in range(e):
+            ans.append(p)
+    print(f"{n}:", " ".join(ans))
