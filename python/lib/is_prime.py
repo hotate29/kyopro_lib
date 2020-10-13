6db61150a834ac5ce461eb69492e9d89ce0f5d0f
hotate29 <44938840+hotate29@users.noreply.github.com>
2020-10-14 06:54:31 +0900

    Update is_prime.py

--- python/lib/is_prime.py
+++ python/lib/is_prime.py
@@ -1,15 +1,8 @@
 def isprime(n: int) -> bool:
-    if n < 2 or n%2 == 0:
+    if n < 2 or n%2 == 0 and not n == 2:
         return False
     for i in range(3, n + 1,2):
         if i * i <= n:
             if n % i == 0:
                 return False
-        else:
-            break
     return True
-
-
-if __name__ == "__main__":
-    for i in range(100):
-        print(i, isprime(i))
