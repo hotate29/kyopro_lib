b5021848bd7be46ce8fdcfbc95433fbbcb8da259
hotate29 <hotate_oc@yahoo.co.jp>
2020-10-13 23:30:15 +0900

    UnionFind

---
+++ lib/UnionFind.py
@@ -0,0 +1,27 @@
+class UnionFind:
+    def __init__(self, n: int) -> None:
+        self.forest = [-1] * n
+
+    def union(self, x: int, y: int) -> None:
+        x = self.findRoot(x)
+        y = self.findRoot(y)
+        if x == y:
+            return
+        if self.forest[x] > self.forest[y]:
+            x, y = y, x
+        self.forest[x] += self.forest[y]
+        self.forest[y] = x
+        return
+
+    def findRoot(self, x: int) -> int:
+        if self.forest[x] < 0:
+            return x
+        else:
+            self.forest[x] = self.findRoot(self.forest[x])
+            return self.forest[x]
+
+    def issame(self, x: int, y: int) -> bool:
+        return self.findRoot(x) == self.findRoot(y)
+
+    def size(self, x: int) -> int:
+        return -self.forest[self.findRoot(x)]
