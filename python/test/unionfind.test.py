085c52a8f86b8720e639d22dd2ac7b184add5998
hotate29 <44938840+hotate29@users.noreply.github.com>
2020-10-14 02:00:21 +0900

    Create unionfind.test.py

---
+++ python/test/unionfind.test.py
@@ -0,0 +1,16 @@
+
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/unionfind
+
+
+
+from python.lib.UnionFind import UnionFind
+
+
+n,q = map(int,input().split())
+uf = UnionFind(n)
+for _ in range(q):
+    ti,ui,vi = map(int,input().split())
+    if ti:
+        print(int(uf.issame(ui,vi)))
+    else:
+        uf.union(ui,vi)
