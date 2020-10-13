e0884d06bd870cd0fccf8a264a5b42b4442a3a1b
hotate29 <44938840+hotate29@users.noreply.github.com>
2020-10-14 06:21:23 +0900

    Create is_prime.test.py

---
+++ python/test/is_prime.test.py
@@ -0,0 +1,7 @@
+# verification-helper: PROBLEM http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_1_C
+
+
+from python.lib.is_prime import isprime
+
+
+print(sum(isprime(int(input()) for _ in range(int(input()))))
