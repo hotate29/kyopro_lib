1d43161ff035a6fdf223792a297dd686a68fb4cd
hotate29 <hotate_oc@yahoo.co.jp>
2020-10-14 07:55:56 +0900

    pep8

--- python/test/unionfind.test.py
+++ python/test/unionfind.test.py
@@ -1,16 +1,14 @@
-
 # verification-helper: PROBLEM https://judge.yosupo.jp/problem/unionfind
-
 
 
 from python.lib.UnionFind import UnionFind
 
 
-n,q = map(int,input().split())
+n, q = map(int, input().split())
 uf = UnionFind(n)
 for _ in range(q):
-    ti,ui,vi = map(int,input().split())
+    ti, ui, vi = map(int, input().split())
     if ti:
-        print(int(uf.issame(ui,vi)))
+        print(int(uf.issame(ui, vi)))
     else:
-        uf.union(ui,vi)
+        uf.union(ui, vi)
