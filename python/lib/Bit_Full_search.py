96fab20e483f61a587150acf9faac273eea7eb23
hotate29 <hotate_oc@yahoo.co.jp>
2020-11-07 21:56:19 +0900

    そこまでジェネレーターにしなくてもええやろ

--- python/lib/Bit_Full_search.py
+++ python/lib/Bit_Full_search.py
@@ -5,4 +5,4 @@ from itertools import product
 
 def BitFullSearch(a):
     p = product((0,1),repeat=len(a))
-    return ((ai for ai,bi in zip(a,pi) if bi) for pi in p)
+    return ([ai for ai,bi in zip(a,pi) if bi] for pi in p)
